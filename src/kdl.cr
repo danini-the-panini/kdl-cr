2a812a89ce7823dc4fb3cf776c4939d72e0db0cb
Danielle Smith <code@danini.dev>
2025-01-30 10:06:20 +0200

    alter public api

--- src/kdl.cr
+++ src/kdl.cr
@@ -3,7 +3,14 @@ require "./kdl/*"
 module KDL
   VERSION = "0.2.0"
 
-  def self.parse_document(string : String)
+  def self.parse(string : String)
     Parser.new.parse(string)
+  end
+
+  def self.load_file(file : IO | String)
+    case file
+    when String then parse(File.read(file))
+    else parse(file.read)
+    end
   end
 end
